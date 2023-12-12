f4e0d402043db9370d8c5cabe4b82a523b29baa2
5ecurealf <alfielloyd12@gmail.com>
2023-12-12 21:59:45 +0000

    feature/frontend : add routing to the app using command npm install react-router-dom, adding the Home/Department/Employee pages

---
+++ FullStackApp/client-app/src/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export class Home extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-8">
+            <h2 className="text-center">Welcome to the Home Page</h2>
+            {/* Other content */}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
