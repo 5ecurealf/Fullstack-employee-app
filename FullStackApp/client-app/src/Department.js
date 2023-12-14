271ba925e8978ae7091db47ac1fb094f347ae628
5ecurealf <alfielloyd12@gmail.com>
2023-12-14 21:54:26 +0000

    feature/frontend : fix to get the modal working

--- FullStackApp/client-app/src/Department.js
+++ FullStackApp/client-app/src/Department.js
@@ -114,45 +114,60 @@ export class Department extends Component {
             ))}
           </tbody>
         </table>
-
+        {/* modal */}
         <div
           className="modal fade"
           id="exampleModal"
           tabIndex="-1"
           aria-hidden="true"
         >
-          <div className="modal-header">
-            <h5 className="modal-title">{modalTitle}</h5>
-            <button
-              type="button"
-              className="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
+          <div className="modal-dialog modal-lg modal-dialog-centered">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">{modalTitle}</h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
 
-          <div className="modal-body">
-            <div className="input-group mb-3">
-              <span className="input-group-text">Department Name</span>
-              <input
-                type="text"
-                className=" form-control"
-                value={DepartmentName}
-                onChange={this.changeDepartmentName}
-              ></input>
+              <div className="modal-body">
+                <div className="input-group mb-3">
+                  <span className="input-group-text">DepartmentName</span>
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={DepartmentName}
+                    onChange={this.changeDepartmentName}
+                  />
+                </div>
+
+                {DepartmentId == 0 ? (
+                  <button
+                    type="button"
+                    className="btn btn-primary float-start"
+                    onClick={() => this.createClick()}
+                  >
+                    Create
+                  </button>
+                ) : null}
+
+                {DepartmentId != 0 ? (
+                  <button
+                    type="button"
+                    className="btn btn-primary float-start"
+                    onClick={() => this.updateClick()}
+                  >
+                    Update
+                  </button>
+                ) : null}
+              </div>
             </div>
-            {DepartmentId == 0 ? (
-              <button type="button" className="btn btn-primary float-start">
-                Create
-              </button>
-            ) : null}
-            {DepartmentId != 0 ? (
-              <button type="button" className="btn btn-primary float-start">
-                Update
-              </button>
-            ) : null}
           </div>
         </div>
+        {/* modal */}
       </div>
     );
   }
