15d5531c7b0e46bedb9b123d74c8077cea70dae5
5ecurealf <alfielloyd12@gmail.com>
2023-12-15 17:58:17 +0000

    feature/frontend : updated the homepage

--- FullStackApp/client-app/src/Home.js
+++ FullStackApp/client-app/src/Home.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
+import "./Home.css";
 
 export class Home extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-md-8">
-            <h2 className="text-center">Welcome to the Home Page</h2>
-            {/* Other content */}
-          </div>
+      <div className="home-container">
+        <div className="hero-content">
+          <h1>Welcome to Our Company</h1>
+          <p>Manage your departments and employees with ease.</p>
         </div>
       </div>
     );
