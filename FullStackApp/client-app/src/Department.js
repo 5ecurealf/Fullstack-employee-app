6118b72fe69acb42f1a7f4c7f78efc74e7ed5d9e
5ecurealf <alfielloyd12@gmail.com>
2023-12-15 16:48:55 +0000

    feature/frontend-filter: department sorting

--- FullStackApp/client-app/src/Department.js
+++ FullStackApp/client-app/src/Department.js
@@ -43,6 +43,18 @@ export class Department extends Component {
     this.setState({ departments: filteredData });
   }
 
+  sortResult(prop, asc) {
+    const sortedData = this.state.departmentsWithoutFilter.sort((a, b) => {
+      if (asc) {
+        return a[prop] > b[prop] ? 1 : a[prop] < b[prop] ? -1 : 0;
+      } else {
+        return b[prop] > a[prop] ? 1 : b[prop] < a[prop] ? -1 : 0;
+      }
+    });
+
+    this.setState({ departments: sortedData });
+  }
+
   changeDepartmentIdFilter = (e) => {
     this.state.DepartmentIdFilter = e.target.value;
     this.FilterFn();
@@ -170,19 +182,99 @@ export class Department extends Component {
           <thead>
             <tr>
               <th>
-                <input
-                  className="form-control m-2"
-                  onChange={this.changeDepartmentIdFilter}
-                  placeholder="Filter"
-                ></input>
+                <div className="d-flex flex-row">
+                  <input
+                    className="form-control m-2"
+                    onChange={this.changeDepartmentIdFilter}
+                    placeholder="Filter"
+                  ></input>
+                  <button
+                    type="button"
+                    className="btn btn-light"
+                    onClick={() => this.sortResult("departmentid", true)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-arrow-down-square"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm8.5 2.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293z"
+                      />
+                    </svg>
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-light"
+                    onClick={() => this.sortResult("departmentid", false)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-arrow-up-square"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm8.5 9.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z"
+                      />
+                    </svg>
+                  </button>
+                </div>
                 DepartmentId
               </th>
               <th>
-                <input
-                  className="form-control m-2"
-                  onChange={this.changeDepartmentNameFilter}
-                  placeholder="Filter"
-                ></input>
+                <div className="d-flex flex-row">
+                  <input
+                    className="form-control m-2"
+                    onChange={this.changeDepartmentNameFilter}
+                    placeholder="Filter"
+                  ></input>
+                  <button
+                    type="button"
+                    className="btn btn-light"
+                    onClick={() => this.sortResult("departmentname", true)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-arrow-down-square"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm8.5 2.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293z"
+                      />
+                    </svg>
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-light"
+                    onClick={() => this.sortResult("departmentname", false)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-arrow-up-square"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm8.5 9.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z"
+                      />
+                    </svg>
+                  </button>
+                </div>
                 DepartmentName
               </th>
               <th>Options</th>
